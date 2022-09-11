583e5cffe67530b53b33da6e75a1b64b4fcd2ff1
Richmond543 <70352481+Richmond543@users.noreply.github.com>
2022-09-11 16:06:20 -0700

    Delete SOS-HGIC-Hostel-Analytica-master directory

--- SOS-HGIC-Hostel-Analytica-master/js/increment.js
+++
@@ -1,9 +0,0 @@
-const db = firebase.firestore();
-
-const increment = firebase.firestore.FieldValue.increment(1);
-const decrement = firebase.firestore.FieldValue.increment(-1);
-
-const storyRef = db.collection('stories').doc('hello-world');
-
-storyRef.update({ count: increment});
-
